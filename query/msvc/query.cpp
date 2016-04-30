aabca528014ac358ea7ba7a57305071f9ca7d1c0
Chatchai Daecha <chkob81@hotmail.com>
2016-04-30 09:39:29 +0000

    add dll project

---
+++ query/msvc/query.cpp
@@ -0,0 +1,22 @@
+// query.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "query.h"
+
+
+// This is an example of an exported variable
+QUERY_API int nquery=0;
+
+// This is an example of an exported function.
+QUERY_API int fnquery(void)
+{
+	return 42;
+}
+
+// This is the constructor of a class that has been exported.
+// see query.h for the class definition
+Cquery::Cquery()
+{
+	return;
+}
